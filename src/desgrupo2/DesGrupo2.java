5d0d54ff015769b4a4c88b021786393921f0a63f
THAMBATO1 <THAMBATO1@THAMBATO>
2016-11-08 22:51:00 -0800

    Primer commit de Desarrollo al repositorio remoto

---
+++ src/desgrupo2/DesGrupo2.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package desgrupo2;
+
+/**
+ *
+ * @author THAMBATO1
+ */
+public class DesGrupo2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
