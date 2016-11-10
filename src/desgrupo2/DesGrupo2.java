c5e9d98590ed8c98ebfa367c519fd4a6af8a8d11
16-LAB6 <16-LAB6@172.21.106.104>
2016-11-10 11:56:21 -0500

    yesse

--- src/desgrupo2/DesGrupo2.java
+++ src/desgrupo2/DesGrupo2.java
@@ -21,7 +21,7 @@ public class DesGrupo2 {
         JOptionPane.showMessageDialog(null,"Prueba");
         System.out.println("hola amiguitos de cora");
         System.out.println("marcos");
-        System.out.println("yesse");
+        System.out.println("yessecarrillo");
          System.out.println("Eddy");
          System.out.println("Jenrry");
                 
