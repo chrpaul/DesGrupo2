449a6b11f1e9e723ecbc40205b9408473c76ed2a
AndresSebastian <AndresSebastian@192.168.1.3>
2016-11-09 20:45:01 -0500

    Hola amigos

--- src/desgrupo2/DesGrupo2.java
+++ src/desgrupo2/DesGrupo2.java
@@ -19,7 +19,7 @@ public class DesGrupo2 {
       char c;
       c=1;
         JOptionPane.showMessageDialog(null,"Prueba");
-        System.out.println("holabb");
+        System.out.println("holabb de cora");
         //hola
     }
 }
